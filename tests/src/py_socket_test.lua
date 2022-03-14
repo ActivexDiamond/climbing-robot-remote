18dd44d5a90efb4a6e420c33d376cd1f5105cb02
ActivexDiamond <activexdiamond@gmail.com>
2022-03-14 16:17:57 +0300

    Pushing to Pi.

--- tests/src/py_socket_test.lua
+++ tests/src/py_socket_test.lua
@@ -1,3 +1,32 @@
+do
+  local bytemarkers = { {0x7FF,192}, {0xFFFF,224}, {0x1FFFFF,240} }
+  function utf8(decimal)
+    if decimal<128 then return string.char(decimal) end
+    local charbytes = {}
+    for bytes,vals in ipairs(bytemarkers) do
+      if decimal<=vals[1] then
+        for b=bytes+1,2,-1 do
+          local mod = decimal%64
+          decimal = (decimal-mod)/64
+          charbytes[b] = string.char(128+mod)
+        end
+        charbytes[1] = string.char(vals[2]+decimal)
+        break
+      end
+    end
+    return table.concat(charbytes)
+  end
+end
+
+local function utf8frompoints(...)
+  local chars,arg={},{...}
+  for i,n in ipairs(arg) do chars[i]=utf8(arg[i]) end
+  return table.concat(chars)
+end
+
+print(utf8frompoints(72, 233, 108, 108, 246, 32, 8364, 8212))
+
+
 local socket = require "socket"
 local json = require "libs.json"
 
@@ -13,13 +42,17 @@ client:send("ping")
 
 print("Entering listening loop.")
 function love.update(dt) 
-	local data, msg
+	local data, err
 	repeat
 		data, err = client:receive()
 		if data then
 			local t = json.decode(data)
-			print("Got data! " .. data, t)
-			for k, v in pairs(t) do
+			print("Got data! " .. data)
+			print()
+			print()
+			print()
+			print("t: " .. t)
+			for k, v in pairs(type(t) == 'table' and t or {}) do
 				print(k, v)
 			end
 		else
