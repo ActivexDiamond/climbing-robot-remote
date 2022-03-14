4405a44ecb252e4d194733782dc5904334a61b18
ActivexDiamond <activexdiamond@gmail.com>
2022-03-14 10:33:04 +0300

    Pushing to Pi.

--- py_tests/server_test.py
+++ py_tests/server_test.py
@@ -22,7 +22,7 @@ msgBytes = bytes(msg, "utf-8")
 
 while 1:
 	print("Listening for ping.")
-	bytesAddressPair = server.recvfrom(bufferSize)
+	bytesAddressPair = server.recvfrom(1024)
 	message = bytesAddressPair[0]
 	address = bytesAddressPair[1]
     
