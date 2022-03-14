29c8e1f10645142115eba24478ade8d447d1ba6c
ActivexDiamond <activexdiamond@gmail.com>
2022-03-14 14:15:51 +0300

    Fixed typo in update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 #Pulls the latest version of the repo for both the remote and robot.
 sudo git reset --hard HEAD
 sudo git pull
-cp -rf /py/TensorFlow.py ~/or/models/research/object_detection/TensorFlow.py
+cp -rf py/TensorFlow.py ~/or/models/research/object_detection/TensorFlow.py
