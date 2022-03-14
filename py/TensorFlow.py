ff5fe6462a57b3cbe456c0137306efc725dd814c
ActivexDiamond <activexdiamond@gmail.com>
2022-03-14 15:23:43 +0300

    Pushing to Pi.

--- py/TensorFlow.py
+++ py/TensorFlow.py
@@ -110,7 +110,7 @@ while True:
             json.dumps(np.squeeze(classes).astype(np.int32)),
             json.dumps(np.squeeze(scores)),
             0.4,                    #min_score_threshold
-            frame)                  #
+            json.dumps(frame))                  #
         sendCnnData(cnnResult)
 		# Draw the results of the detection (aka 'visulaize the results')
         #vis_util.visualize_boxes_and_labels_on_image_array(
