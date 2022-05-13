fc9dc6a248306be0d8d1b67527c4a300aecf61dc
ActivexDiamond <activexdiamond@gmail.com>
2022-05-13 19:44:47 +0300

    Added gearboxes to the Nano's code. Untested!

--- climbing_robot_nano_api/climbing_robot_nano_api.ino
+++ climbing_robot_nano_api/climbing_robot_nano_api.ino
@@ -1,139 +1,239 @@
+/// Libs
 #include <Servo.h>
-Servo rightWheel;
-Servo leftWheel;
-#define rWheel_pin A1
-#define lWheel_pin A0
 
-#define cutterWorm_pin A3
-#define cutterWheel_pin A2
-Servo cutterWorm;
-Servo cutterWheel;
-#define wormOpen 170
-#define wormClose 10
+/// Config
+#define DEBUG true
 
-//#define bluetooth Serial
-//SoftwareSerial bluetooth(2,7);
-#define debug true
+/// Pins
+#define R_WHEEL_PIN A1
+#define L_WHEEL_PIN A0
+
+#define CUTTER_WORM_PIN A3
+#define CUTTER_WHEEL_PIN A2
 
 #define UD_LPWM 6
 #define UD_RPWM 5
 #define FB_LPWM 11
 #define FB_RPWM 3
 
-//#define trigger 8
-//#define echo 9
-//unsigned long usT = 0;
-//Ultrasonic ultrasonic(trigger, echo);
+//Gearbox motor pins.
+#define R_GEARBOX_PIN = 9
+#define L_GEARBOX_PIN = 10
 
+/// Formula-esc Constants
+#define WORM_OPEN 170
+#define WORM_CLOSE 10
+
+/// State
+Servo rWheel;
+Servo lWheel;
+
+Servo cutterWorm;
+Servo cutterWheel;
+
+/// Core API
 void setup() {
-  pinMode(UD_LPWM,OUTPUT); pinMode(UD_RPWM,OUTPUT); pinMode(FB_LPWM,OUTPUT); pinMode(FB_RPWM,OUTPUT);
-  rightWheel.attach(rWheel_pin);
-  leftWheel.attach(lWheel_pin);
-  cutterWorm.attach(cutterWorm_pin);
-  cutterWheel.attach(cutterWheel_pin);
-  rightWheel.write(90); leftWheel.write(90);
-  cutterWorm.write(wormClose); cutterWheel.write(0);
-  //bluetooth.begin(9600);
-  Serial.begin(9600);
-//  usT = millis();
+    //Serial Setup
+    Serial.begin(9600);
 
-  ARM_Stop();
-  Wheel_Stop();
-  cutterWorm.write(wormClose);
-  cutterWheel.write(0);
+    //Pin Setup
+    Serial.println("Entering setup...")
+    pinMode(UD_LPWM, OUTPUT);
+    pinMode(UD_RPWM, OUTPUT);
+    pinMode(FB_LPWM, OUTPUT);
+    pinMode(FB_RPWM, OUTPUT);
+
+    pinMode(R_GEARBOX_PIN, OUTPUT);
+    pinMode(L_GEARBOX_PIN, OUTPUT);
+
+    //Servo Setup
+    rWheel.attach(R_WHEEL_PIN);
+    lWheel.attach(L_WHEEL_PIN);
+
+    cutterWorm.attach(CUTTER_WORM_PIN);
+    cutterWheel.attach(CUTTER_WHEEL_PIN);
+
+    //Init to defaults.
+    rWheel.write(90);
+    lWheel.write(90);
+
+    cutterWorm.write(wormClose);
+    cutterWheel.write(0);
+
+    ARM_Stop();
+    Wheel_Stop();
+
+    //Echo
+    Serial.println("Setup complete.")
 }
 
 void loop() {
-  while(Serial.available()){
-    String c = Serial.readStringUntil(';');
-    if(c=="AS") ARM_Stop();
-    if(c=="AU" || c=="AD" || c=="AF" || c=="AB"){  
-      int s = Serial.readStringUntil(';').toInt();
-      if(c == "AU") ARM_Up(s);
-      if(c == "AD") ARM_Down(s);
-      if(c == "AF") ARM_Forward(s);
-      if(c == "AB") ARM_Backward(s);
+    //Poll for cmds.
+    while(Serial.available()) {
+        //Fetch first cmd.
+        String c = Serial.readStringUntil(';');
+        //Arm. [S]
+        if(c=="AS") ARM_Stop();
+        //Arm with speed. [U/D/F/B]
+        if(c=="AU" || c=="AD" || c=="AF" || c=="AB") {
+          int s = Serial.readStringUntil(';').toInt();
+          if(c == "AU") ARM_Up(s);
+          if(c == "AD") ARM_Down(s);
+          if(c == "AF") ARM_Forward(s);
+          if(c == "AB") ARM_Backward(s);
+        }
+
+        //Wheel. [S/R/L]
+        if(c=="WS") Wheel_Stop();
+        if(c == "WR") Wheel_Right();
+        if(c == "WL") Wheel_Left();
+        //Wheel with degree. [F/B]
+        if(c=="WF" || c=="WB") {
+          int val = Serial.readStringUntil(';').toInt();
+          if(c == "WF") Wheel_Forward(val);
+          if(c == "WB") Wheel_Backward(val);
+        }
+
+        //CutterWorm with degree.
+        if(c == "CWorm") {
+          int d = Serial.readStringUntil(';').toInt();
+          d = map(d, 0, 5, wormClose, WORM_OPEN);
+          cutterWorm.write(d);
+        }
+
+        //CutterWheel with degree.
+        if(c == "CWheel") {
+          int d = Serial.readStringUntil(';').toInt();
+          d = map(d, 0, 10, 0, 180);
+          cutterWheel.write(d);
+        }
     }
-    if(c=="WS") Wheel_Stop();
-    if(c == "WR") Wheel_Right();
-    if(c == "WL") Wheel_Left();
-    if(c=="WF" || c=="WB"){
-      int d = Serial.readStringUntil(';').toInt();
-      d = map(d,75,255,90,0);
-      if(c == "WF") Wheel_Forward(d);
-      if(c == "WB") Wheel_Backward(d);
+}
+
+void ARM_Stop() {
+    analogWrite(UD_RPWM, 0); analogWrite(UD_LPWM, 0);
+    analogWrite(FB_RPWM, 0); analogWrite(FB_LPWM, 0);
+
+    if(DEBUG) {
+        Serial.println("ARM_Stop");
     }
-    if(c == "CWorm"){
-      int d = Serial.readStringUntil(';').toInt();
-      d = map(d,0,5,wormClose,wormOpen);
-      cutterWorm.write(d);
+}
+
+void ARM_Up(int spd) {
+    analogWrite(UD_RPWM, 0);
+    analogWrite(UD_LPWM, 0);
+
+    analogWrite(UD_RPWM, spd);
+
+    if(DEBUG) {
+        Serial.print("ARM_Up  spd=");
+        Serial.println(spd);
     }
-    if(c == "CWheel"){
-      int d = Serial.readStringUntil(';').toInt();
-      d = map(d,0,10,0,180);
-      cutterWheel.write(d);      
+}
+
+void ARM_Down(int spd) {
+    analogWrite(UD_RPWM, 0);
+    analogWrite(UD_LPWM, 0);
+
+    analogWrite(UD_LPWM, spd);
+
+    if(DEBUG) {
+        Serial.print("ARM_Down  spd=");
+        Serial.println(spd);
     }
-  }
-//  if(millis()> usT + 500){
-//    int cm = 0;
-//    while(cm==0) cm = ultrasonic.distanceRead();
-//    bluetooth.print("us;" + String(cm) + ";");
-//    if(debug) Serial.println("us;" + String(cm) + ";");
-//    usT = millis();
-//  }
 }
 
-void ARM_Stop(){
-  analogWrite(UD_RPWM, 0); analogWrite(UD_LPWM, 0);
-  analogWrite(FB_RPWM, 0); analogWrite(FB_LPWM, 0);
-  if(debug) Serial.println("ARM_Stop");
+void ARM_Forward(int spd) {
+    analogWrite(FB_RPWM, 0);
+    analogWrite(FB_LPWM, 0);
+
+    analogWrite(FB_RPWM, spd);
+
+    if(DEBUG) {
+        Serial.print("ARM_Forward  spd=");
+        Serial.println(spd);
+    }
 }
 
-void ARM_Up(int spd){
-  analogWrite(UD_RPWM, 0); analogWrite(UD_LPWM, 0);
-  analogWrite(UD_RPWM, spd);
-  if(debug) {Serial.print("ARM_Up  spd="); Serial.println(spd);}
+void ARM_Backward(int spd) {
+    analogWrite(FB_RPWM, 0);
+    analogWrite(FB_LPWM, 0);
+
+    analogWrite(FB_LPWM, spd);
+
+    if(DEBUG) {
+        Serial.print("ARM_Backward  spd=");
+        Serial.println(spd);
+    }
 }
 
-void ARM_Down(int spd){
-  analogWrite(UD_RPWM, 0); analogWrite(UD_LPWM, 0);
-  analogWrite(UD_LPWM, spd);
-  if(debug) {Serial.print("ARM_Down  spd="); Serial.println(spd);}
+void Wheel_Stop() {
+    rWheel.write(90);
+    lWheel.write(90);
+
+    analogWrite(R_GEARBOX_PIN, 0);
+    analogWrite(L_GEARBOX_PIN, 0);
+
+    if(DEBUG) {
+        Serial.println("Wheel_Stop");
+    }
 }
 
-void ARM_Forward(int spd){
-  analogWrite(FB_RPWM, 0); analogWrite(FB_LPWM, 0);
-  analogWrite(FB_RPWM, spd);
-  if(debug) {Serial.print("ARM_Forward  spd="); Serial.println(spd);}
+void Wheel_Forward(int val) {
+    inv deg = map(val, 75, 255, 90, 0);
+    int spd = map(val, 75, 255, 0, 255);
+
+    rWheel.write(deg);
+    lWheel.write(180-deg);
+
+    analogWrite(R_GEARBOX_PIN, spd);
+    analogWrite(L_GEARBOX_PIN, spd);
+
+    if(DEBUG) {
+        Serial.print("Wheel_Forward  degR=");
+        Serial.print(deg);
+        Serial.print("  degL=");
+        Serial.println(180-deg);
+    }
 }
 
-void ARM_Backward(int spd){
-  analogWrite(FB_RPWM, 0); analogWrite(FB_LPWM, 0);
-  analogWrite(FB_LPWM, spd);
-  if(debug) {Serial.print("ARM_Backward  spd="); Serial.println(spd);}
+void Wheel_Backward(int val) {
+    inv deg = map(val, 75, 255, 90, 0);
+    int spd = map(val, 75, 255, 0, 255);
+
+    rWheel.write(180-deg);
+    lWheel.write(deg);
+
+    analogWrite(R_GEARBOX_PIN, spd);
+    analogWrite(L_GEARBOX_PIN, spd);
+
+    if(DEBUG) {Serial.print("Wheel_Backward  degR=");
+        Serial.print(180-deg);
+        Serial.print("  degL=");
+        Serial.println(deg);
+    }
 }
 
-void Wheel_Stop(){
-  rightWheel.write(90); leftWheel.write(90);
-  if(debug) {Serial.println("Wheel_Stop");}
+void Wheel_Right() {
+    rWheel.write(180);
+    lWheel.write(180);
+
+    analogWrite(R_GEARBOX_PIN, 0);
+    analogWrite(L_GEARBOX_PIN, 255);
+
+    if(DEBUG) {
+        Serial.println("Wheel_Right");
+    }
 }
 
-void Wheel_Forward(int deg){
-  rightWheel.write(deg); leftWheel.write(180-deg);
-  if(debug) {Serial.print("Wheel_Forward  degR="); Serial.print(deg); Serial.print("  degL="); Serial.println(180-deg);}
-}
+void Wheel_Left() {
+    rWheel.write(0);
+    lWheel.write(0);
 
-void Wheel_Backward(int deg){
-  rightWheel.write(180-deg); leftWheel.write(deg);
-  if(debug) {Serial.print("Wheel_Backward  degR="); Serial.print(180-deg); Serial.print("  degL="); Serial.println(deg);}
-}
+    analogWrite(R_GEARBOX_PIN, 255);
+    analogWrite(L_GEARBOX_PIN, 0);
 
-void Wheel_Right(){
-  rightWheel.write(180); leftWheel.write(180);
-  if(debug) {Serial.println("Wheel_Right");}
-}
-
-void Wheel_Left(){
-  rightWheel.write(0); leftWheel.write(0);
-  if(debug) {Serial.println("Wheel_Left");}
+    if(DEBUG) {
+        Serial.println("Wheel_Left");
+    }
 }
